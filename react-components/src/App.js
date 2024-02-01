21017ea64ecbea6d7fb7bc921ae4ef57e6a3499f
Nyakamba <omwegaenock@gmail.com>
2024-02-01 17:09:39 +0300

    functional components

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome home</h1> <h1>Welcome home</h1>
+      <h1>Welcome home</h1>
+      <h1>Welcome home</h1>
+      <h1>Welcome home</h1>
     </div>
   );
 }
