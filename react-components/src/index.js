d7cbd44a26abfdaf68c9a2e11b5009237f5969b9
Nyakamba <omwegaenock@gmail.com>
2024-02-01 17:44:27 +0300

    CssInReact

--- react-components/src/index.js
+++ react-components/src/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Message from "./component/Message";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Message />
+    <App />
   </React.StrictMode>
 );
