5055e26e44d410ec8f26c8cbb7f378f56b707625
Nyakamba <omwegaenock@gmail.com>
2024-02-11 14:24:47 +0300

    usereducer

--- reactcontextapi/src/App.js
+++ reactcontextapi/src/App.js
@@ -1,10 +1,12 @@
 import AddTodo from "./components/AddTodo";
 import "./components/AddTodo.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div>
       <AddTodo />
+      <TodoList />
     </div>
   );
 }
